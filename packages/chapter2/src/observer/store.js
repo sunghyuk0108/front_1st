146c6ef55a85999ac52d80e6bafc0de4c92f7ed8
momo <harlie0108@gmail.com>
2024-03-30 00:11:23 +0900

    STEP5 과제F_박성혁

--- packages/chapter2/src/observer/store.js
+++ packages/chapter2/src/observer/store.js
@@ -1,6 +1,14 @@
+import { 발행기관 } from "./pubsub";
+
 export class Store {
-  constructor({ state, mutations, actions }) {}
+  constructor({ state, mutations, actions }) {
+    this.state = 발행기관(state);
+    this.mutations = mutations;
+    this.actions = actions;
+  }
 
-  commit(action, payload) {}
-
+  commit(action, payload) {
+    console.log(action, payload);
+    this.mutations[action](this.state, payload);
+  }
 }
