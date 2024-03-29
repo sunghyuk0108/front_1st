f48018e6ce940febb9657bdbe50c74074259fe21
momo <harlie0108@gmail.com>
2024-03-30 00:11:06 +0900

    STEP5 과제E_박성혁

--- packages/chapter2/src/observer/pubsub.js
+++ packages/chapter2/src/observer/pubsub.js
@@ -1,7 +1,28 @@
-export const 구독 = fn => {
-  fn();
-}
+let currentCallback = null;
 
-export const 발행기관 = obj => {
-  return obj;
-}
+export const 구독 = (fn) => {
+  currentCallback = fn;
+  fn();
+};
+
+export const 발행기관 = (obj) => {
+  const state = {};
+
+  for (const [key, value] of Object.entries(obj)) {
+    let initState = value;
+    let observer = new Set();
+
+    Object.defineProperty(state, key, {
+      get() {
+        observer.add(currentCallback);
+        return initState;
+      },
+      set(value) {
+        initState = value;
+        observer.forEach((x) => x());
+      },
+    });
+  }
+
+  return state;
+};
